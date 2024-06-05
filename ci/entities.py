1a94d05ea3ae4acc8f9b8ec32caea91f42318806
Euan Goddard <euangoddard@manufacture2030.com>
2024-06-05 11:43:56 +0100

    feat: add support for non-working days

--- ci/entities.py
+++ ci/entities.py
@@ -1,5 +1,28 @@
 from dataclasses import dataclass
 from datetime import date
+from calendar import MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY
+
+
+@dataclass(frozen=True)
+class WorkingDays:
+    mon: bool
+    tue: bool
+    wed: bool
+    thu: bool
+    fri: bool
+
+    def works_on(self, day: int) -> bool:
+        if day == MONDAY:
+            return self.mon
+        if day == TUESDAY:
+            return self.tue
+        if day == WEDNESDAY:
+            return self.wed
+        if day == THURSDAY:
+            return self.thu
+        if day == FRIDAY:
+            return self.fri
+        return False
 
 
 @dataclass(frozen=True)
@@ -10,3 +33,16 @@ class ContractorData:
     rate: int
     start_date: date
     holiday_days: float
+    working_days: WorkingDays
+
+    @staticmethod
+    def from_data(data: tuple) -> "ContractorData":
+        return ContractorData(
+            data[0],
+            data[1],
+            data[2],
+            data[3],
+            data[4],
+            data[5],
+            WorkingDays(*data[6:]),
+        )
--- ci/report.py
+++ ci/report.py
@@ -2,7 +2,7 @@ from calendar import monthrange, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY
 from datetime import date
 from pathlib import Path
 from typing import Generator
-from ci.entities import ContractorData
+from ci.entities import ContractorData, WorkingDays
 import duckdb
 
 WEEK_DAYS = frozenset((MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY))
@@ -11,12 +11,19 @@ HOLIDAYS_TABLE_NAME = "holidays"
 
 RATES_TABLE_NAME = "rates"
 
+DAYS_TABLE_NAME = "days"
+
 
 def generate_report(year: int, month: int) -> None:
     contractor_data = _load_data()
 
     for item in contractor_data:
-        working_days = _get_working_days_in_month(year, month, item.start_date)
+        working_days = _get_working_days_in_month(
+            year,
+            month,
+            item.start_date,
+            item.working_days,
+        )
         days_worked = working_days - item.holiday_days
         print(
             f"Data for employee: {item.employee_id} ({item.first_name} {item.last_name})"
@@ -32,6 +39,7 @@ def generate_report(year: int, month: int) -> None:
 def _load_data() -> Generator[ContractorData, None, None]:
     _setup_table(_get_data_path("holidays.csv"), HOLIDAYS_TABLE_NAME)
     _setup_table(_get_data_path("rates.csv"), RATES_TABLE_NAME)
+    _setup_table(_get_data_path("days.csv"), DAYS_TABLE_NAME)
 
     sql = f"""
 SELECT 
@@ -43,12 +51,17 @@ SELECT
     COALESCE(
         (SELECT SUM("Holiday Duration (Days)") FROM {HOLIDAYS_TABLE_NAME} WHERE "Employee Id" = rates."Employee Id"), 
         0
-    )
-FROM {RATES_TABLE_NAME};
+    ),
+    COALESCE(days.Mon, TRUE),
+    COALESCE(days.Tue, TRUE),
+    COALESCE(days.Wed, TRUE),
+    COALESCE(days.Thu, TRUE),
+    COALESCE(days.Fri, TRUE)
+FROM {RATES_TABLE_NAME} LEFT JOIN {DAYS_TABLE_NAME} ON rates."Employee Id" = days."Employee Id" ORDER BY rates."First Name", rates."Last Name"
 """
 
     for row in duckdb.sql(sql).fetchall():
-        yield ContractorData(*row)
+        yield ContractorData.from_data(row)
 
 
 def _get_data_path(file_name: str) -> str:
@@ -61,11 +74,25 @@ def _setup_table(file_path: str, table_name: str) -> None:
     )
 
 
-def _get_working_days_in_month(year: int, month: int, start_date: date) -> float:
+def _get_working_days_in_month(
+    year: int,
+    month: int,
+    start_date: date,
+    contractor_work_days: WorkingDays,
+) -> float:
     days_in_month = monthrange(year, month)[1]
     days = [date(year, month, day) for day in range(1, days_in_month + 1)]
     week_days_in_month = sum(
-        [1 if d.weekday() in WEEK_DAYS and start_date <= d else 0 for d in days]
+        [
+            (
+                1
+                if d.weekday() in WEEK_DAYS
+                and contractor_work_days.works_on(d.weekday())
+                and start_date <= d
+                else 0
+            )
+            for d in days
+        ]
     )
 
     return float(week_days_in_month)
